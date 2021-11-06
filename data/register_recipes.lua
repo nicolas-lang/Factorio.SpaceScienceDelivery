9f63344fdf725bd0ab21dcf518e6dcf2aebff53f
Nicolas Lang <git@nicolas-lang.de>
2021-11-06 18:18:49 +0100

    auto-code-formatter

--- data/register_recipes.lua
+++ data/register_recipes.lua
@@ -2,7 +2,7 @@
 local data_util = require("data_util")
 --=================================================================================================
 local itemBoxName
-for _, item in pairs (data.raw["tool"]) do
+for _, item in pairs(data.raw["tool"]) do
 	local txt = item.name
 	local recipe = data_util.getRecipe(item.name)
 	local icon = item.icon or item.icons[0]
@@ -19,24 +19,24 @@ for _, item in pairs (data.raw["tool"]) do
 				newItem.order = "zz" .. item.order
 				local overlayIcon = {
 					icon = "__nco-SpaceScienceDelivery__/graphics/icons/icon_P.png",
-					icon_size = 64,
+					icon_size = 64
 				}
 				if item.icons then
-					newItem.icons =  util.table.deepcopy(item.icons)
-					table.insert(newItem.icons,overlayIcon)
+					newItem.icons = util.table.deepcopy(item.icons)
+					table.insert(newItem.icons, overlayIcon)
 				else
 					local baseIcon = {
-						icon=item.icon,
+						icon = item.icon,
 						icon_size = item.icon_size
 					}
-					newItem.icons = {baseIcon,overlayIcon}
+					newItem.icons = {baseIcon, overlayIcon}
 				end
 				newItem.icon = nil
 				data.raw.item[itemBoxName] = newItem
 				---------------------------------------------------------------------------------------
 				local bulletRecipeName = "se-delivery-cannon-pack-" .. "crate-of-" .. item.name
 				local bulletRecipe = data.raw.recipe[bulletRecipeName]
-				--log(serpent.block( bulletRecipe, {comment = false, numformat = '%1.8g' } ))
+
 				if bulletRecipe then
 					bulletRecipe.icon = nil
 					bulletRecipe.icon_mipmaps = nil
@@ -52,32 +52,32 @@ for _, item in pairs (data.raw["tool"]) do
 				boxRecipe.icons = newItem.icons
 				boxRecipe.result = nil
 				boxRecipe.result_count = nil
-				local results = {{amount = 1,name = itemBoxName,type = "item"}}
+				local results = {{amount = 1, name = itemBoxName, type = "item"}}
 				local extraIngredient = {type = "item", name = "se-heat-shielding", amount = 1}
 				if boxRecipe.normal then
-					table.insert(boxRecipe.normal.ingredients,extraIngredient)
+					table.insert(boxRecipe.normal.ingredients, extraIngredient)
 					boxRecipe.normal.result = nil
-					boxRecipe.normal.result_count=nil
+					boxRecipe.normal.result_count = nil
 					boxRecipe.normal.results = results
 				end
 				if boxRecipe.expensive then
-					table.insert(boxRecipe.expensive.ingredients,extraIngredient)
+					table.insert(boxRecipe.expensive.ingredients, extraIngredient)
 					boxRecipe.expensive.result = nil
-					boxRecipe.expensive.result_count=nil
+					boxRecipe.expensive.result_count = nil
 					boxRecipe.expensive.results = results
 				end
 				if boxRecipe.normal == nil then
 					boxRecipe.results = results
 					if boxRecipe.ingredients then
-						table.insert(boxRecipe.ingredients,extraIngredient)
+						table.insert(boxRecipe.ingredients, extraIngredient)
 					end
 				end
-				--log(serpent.block(boxRecipe, {comment = false, numformat = '%1.8g' } ))
+
 				data:extend({boxRecipe})
 				---------------------------------------------------------------------------------------
 				for k, v in pairs(data.raw.module) do
 					if v.limitation then
-						if data_util.has_value(v.limitation,recipe.name) then
+						if data_util.has_value(v.limitation, recipe.name) then
 							table.insert(v.limitation, boxRecipe.name)
 							log("added recipe " .. boxRecipe.name .. " to  " .. v.name .. " limitations")
 						end
@@ -101,8 +101,8 @@ for _, item in pairs (data.raw["tool"]) do
 				data:extend({unboxRecipe})
 				local tech = data_util.getTechnologyForRecipe(recipe.name)
 				if tech then
-					table.insert(tech.effects,{ type = "unlock-recipe", recipe = boxRecipe.name})
-					table.insert(tech.effects,{ type = "unlock-recipe", recipe = unboxRecipe.name})
+					table.insert(tech.effects, {type = "unlock-recipe", recipe = boxRecipe.name})
+					table.insert(tech.effects, {type = "unlock-recipe", recipe = unboxRecipe.name})
 					data.raw["technology"][tech.name] = tech
 				else
 					txt = txt .. "tech not found"
